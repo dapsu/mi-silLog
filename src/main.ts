361b9f55ce5ab975deee9b2052f1fdb9441d94c5
dapsu <greenrock46@gmail.com>
2023-03-24 21:16:35 +0900

    feat: http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@src/app.module';
+import { HttpExceptionFilter } from '@src/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT;
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(PORT);
 }
 bootstrap();
