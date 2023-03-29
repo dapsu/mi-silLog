4bb443c1fd97ff0af7c21519e5bbffb4b9ca71ee
dapsu <greenrock46@gmail.com>
2023-03-29 16:19:07 +0900

    feat: add prisma module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,13 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthModule } from '@src/modules/auth/auth.module';
+import { PrismaService } from '@src/prisma/prisma.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [forwardRef(() => AuthModule)],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PrismaService],
   exports: [UsersService],
 })
 export class UsersModule {}
