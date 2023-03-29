16ef9b872fbb7605caa757cc386791b49a7c9efe
dapsu <greenrock46@gmail.com>
2023-03-29 16:17:07 +0900

    refactor: replace importing modules with modules/index.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 import { LoggerMiddleware } from '@src/middlewares/logger/logger.middleware';
-import { AuthModule } from '@src/modules/auth/auth.module';
-import { UsersModule } from '@src/modules/users/users.module';
+import { modules } from '@src/modules';
+import { PrismaService } from '@src/prisma/prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot(), ...modules],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
