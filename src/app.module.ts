e2eb9a36bee99d0ca8a82792a700607e52b371d6
dapsu <greenrock46@gmail.com>
2023-03-29 19:43:11 +0900

    refactor: create prisma module in /modules dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,11 @@ import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 import { LoggerMiddleware } from '@src/middlewares/logger/logger.middleware';
 import { modules } from '@src/modules';
-import { PrismaService } from '@src/prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), ...modules],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,5 @@
 import { UsersModule } from '@src/modules/users/users.module';
 import { AuthModule } from '@src/modules/auth/auth.module';
+import { PrismaModule } from '@src/modules/prisma/prisma.module';
 
-export const modules = [UsersModule, AuthModule];
+export const modules = [UsersModule, AuthModule, PrismaModule];
